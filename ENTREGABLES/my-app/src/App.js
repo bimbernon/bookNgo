6062ea3b32ef9641ace0fa592b54b9a04839402e
Barbara <barbara.if93@hotmail.com>
2021-02-26 12:13:34 +0100

    creacion proyecto frontend

---
+++ ENTREGABLES/my-app/src/App.js
@@ -0,0 +1,80 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { NavigationMenu } from "./components/header/NavigationMenu";
+import { Home } from "./pages/home";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavigationMenu />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export { App };
+
+/* <nav>
+          <ul>
+            <li>
+              <Link to="/api/v1/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/users" className="link">
+                Users
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/books" className="link">
+                Books
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/authors" className="link">
+                Authors
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/cathegories" className="link">
+                Cathegories
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/reserves" className="link">
+                Reserves
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/donations" className="link">
+                Donations
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/cards" className="link">
+                Cards
+              </Link>
+            </li>
+            <li>
+              <Link to="/api/v1/invoices" className="link">
+                Invoices
+              </Link>
+            </li>
+          </ul>
+        </nav> */
+
+/* <Route path="/api/v1/users"></Route>
+          <Route path="/api/v1/books"></Route>
+          <Route path="/api/v1/authors"></Route>
+          <Route path="/api/v1/cathegories"></Route>
+          <Route path="/api/v1/reserves"></Route>
+          <Route path="/api/v1/donations"></Route>
+          <Route path="/api/v1/cards"></Route>
+          <Route path="/api/v1/invoices"></Route> */
---
+++ ENTREGABLES/my-app/src/components/header/NavigationMenu.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavigationMenu = () => {
+  return (
+    <nav className="navigationMenu">
+      <ul>
+        <li>
+          <Link to="/" className="link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/donations" className="link">
+            Dona tus libros
+          </Link>
+        </li>
+        <li>
+          <Link to="/users/register" className="link">
+            Regístrate
+          </Link>
+        </li>
+        <li>
+          <Link to="/users/login" className="link">
+            Inicia sesión
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { NavigationMenu };
