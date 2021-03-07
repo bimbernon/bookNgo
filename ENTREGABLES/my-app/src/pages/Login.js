b3457c16dac222f10ce56890381550c91da89980
Jose Lopez Fernandez <joselopezfernandez@MacBook-Pro-de-Jose.local>
2021-03-07 20:39:24 +0100

    Se consigue guardar el token en el localSotage, y se genera un mensaje de error en caso de que no existe el login o contraseña

--- ENTREGABLES/my-app/src/pages/Login.js
+++ ENTREGABLES/my-app/src/pages/Login.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Redirect } from "react-router-dom";
 import { LoginForm } from "../components/main/LoginMain/LoginForm/LoginForm";
-
+import { AuthContext } from "../components/providers/AuthProvider";
 export const Login = () => {
+  const [token, setToken] = useContext(AuthContext);
+  const doSuccess = (responseBody) => setToken(responseBody.accessToken);
+  const doError = (error) => console.error(error);
+  console.log(token);
+  if (token !== "") {
+    <Redirect to="/"></Redirect>;
+  }
   return (
-    <Router>
-      <div>
-        <LoginForm></LoginForm>
-      </div>
-    </Router>
+    <div>
+      <LoginForm
+        url="http://localhost:3080/api/v1/users/login/"
+        onSuccess={doSuccess}
+        onError={doError}
+      ></LoginForm>
+    </div>
   );
 };
